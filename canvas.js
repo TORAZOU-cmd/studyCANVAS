d5ddaa61f9fd419f794052f4f97b4823f20dc033
TORAZOU-cmd <106525286+TORAZOU-cmd@users.noreply.github.com>
2022-06-17 15:44:21 +0900

    paddle and keyboard controls

--- canvas.js
+++ canvas.js
@@ -6,10 +6,52 @@ let y = canvas.height - 30;
 let dx = 2;
 let dy = -2;
 const ballRadius = 10;
+const paddleHeight = 10;
+const paddleWidth = 75;
+let paddleX = (canvas.width - paddleWidth) / 2;
+let rightPressed = false;
+let leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyupHandler, false);
+
+function keyDownHandler(e) {
+  switch (e.key) {
+    case "Right":
+    case "ArrowRight":
+      rightPressed = true;
+      break;
+    case "Left":
+    case "ArrowLeft":
+      leftPressed = true;
+      break;
+  }
+}
+
+function keyupHandler(e) {
+  switch (e.key) {
+    case "Right":
+    case "ArrowRight":
+      rightPressed = false;
+      break;
+    case "Left":
+    case "ArrowLeft":
+      leftPressed = false;
+      break;
+  }
+}
 
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+  ctx.fillStyle = "#0095dd";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
   ctx.fillStyle = "#0095dd";
   ctx.fill();
   ctx.closePath();
@@ -20,7 +62,14 @@ function draw() {
   if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) dy = -dy;
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawPaddle();
   x += dx;
   y += dy;
+  if (rightPressed && paddleX < canvas.width - paddleWidth) {
+    paddleX += 7;
+  }
+  if (leftPressed && paddleX > 0) {
+    paddleX -= 7;
+  }
 }
 setInterval(draw, 10);
