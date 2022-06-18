f82953e9f064fce6ccb08aa23526f39a434e9b65
TORAZOU-cmd <106525286+TORAZOU-cmd@users.noreply.github.com>
2022-06-18 15:40:04 +0900

    CollisionDetection

--- canvas.js
+++ canvas.js
@@ -23,7 +23,7 @@ const bricks = [];
 for (let c = 0; c < brickColumnCount; c++) {
   bricks[c] = [];
   for (let r = 0; r < brickRowCount; r++) {
-    bricks[c][r] = { x: 0, y: 0 };
+    bricks[c][r] = { x: 0, y: 0, status: 1 };
   }
 }
 
@@ -56,6 +56,25 @@ function keyupHandler(e) {
   }
 }
 
+function collisionDetection() {
+  for (let c = 0; c < brickColumnCount; c++) {
+    for (let r = 0; r < brickRowCount; r++) {
+      const b = bricks[c][r];
+      if (b.status == 1) {
+        if (
+          x > b.x &&
+          x < b.x + brickWidth &&
+          y > b.y &&
+          y < b.y + brickHeight
+        ) {
+          dy = -dy;
+          b.status = 0;
+        }
+      }
+    }
+  }
+}
+
 function drawBall() {
   ctx.beginPath();
   ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
@@ -75,15 +94,17 @@ function drawPaddle() {
 function drawBricks() {
   for (let c = 0; c < brickColumnCount; c++) {
     for (let r = 0; r < brickRowCount; r++) {
-      const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
-      const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
-      bricks[c][r].x = brickX;
-      bricks[c][r].y = brickY;
-      ctx.beginPath();
-      ctx.rect(brickX, brickY, brickWidth, brickHeight);
-      ctx.fillStyle = "#0095dd";
-      ctx.fill();
-      ctx.closePath();
+      if (bricks[c][r].status == 1) {
+        const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+        const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+        bricks[c][r].x = brickX;
+        bricks[c][r].y = brickY;
+        ctx.beginPath();
+        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+        ctx.fillStyle = "#0095dd";
+        ctx.fill();
+        ctx.closePath();
+      }
     }
   }
 }
@@ -105,6 +126,7 @@ function draw() {
   drawBall();
   drawPaddle();
   drawBricks();
+  collisionDetection();
   x += dx;
   y += dy;
   if (rightPressed && paddleX < canvas.width - paddleWidth) {
@@ -114,4 +136,5 @@ function draw() {
     paddleX -= 7;
   }
 }
+
 const interval = setInterval(draw, 10);
